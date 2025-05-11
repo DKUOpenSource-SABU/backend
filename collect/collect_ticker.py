b0ab7c60263061a69da47fd0b40f1d6443b4257b
Kim-Taehyeong <taehyeok02@gmail.com>
2025-05-11 10:14:43 +0900

    ✨feat : 수집 기능 추가
    1. Nasdaq에서 TICKER 및 최신 현재가 수집
    2. vantage_api에서 Ticker별 Historical Data 수집
    3. tiingo에서 Ticker별 Historical Data 수집
    
    저장된 Ticker별 데이터는 ./stock에 순차적으로 저장. 약 1시간 단위로 50개 제한 일 1000개까지 가능
    
    총 약 3500개 다운 필요 -> 예상 약 3~4일 소요 예정

---
+++ collect/collect_ticker.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+def download_etf_csv():
+    script_path = os.path.abspath("nasdaq-download.js")
+    result = subprocess.run(["node", script_path],
+                             capture_output=True,
+                             text=True)
+
+    print(result.stdout)
+    if result.returncode != 0:
+        print("❌ 오류 발생:")
+        print(result.stderr)
+
+download_etf_csv()
