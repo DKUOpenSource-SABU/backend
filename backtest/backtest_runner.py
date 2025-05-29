00cde3c607b28771d77da12cffdd725e6c5214f6
Kim-Taehyeong <taehyeok02@gmail.com>
2025-05-30 02:24:51 +0900

    ✨feat : 리더보드 기능 추가
     리더보드 기능 추가 완료
    
     Close #45

--- backtest/backtest_runner.py
+++ backtest/backtest_runner.py
@@ -4,6 +4,8 @@ from backtest.rsi import Rsi
 from backtest.data_provider import load_data
 from backtest.utils import calculate_metrics
 from models.request_models import BacktestRequest
+from core.db import update_max_strategy
+import copy
 
 STRATEGIES = [BuyAndHold, SmaCross, Rsi]
 
@@ -13,6 +15,7 @@ def run_backtest(req: BacktestRequest):
     data = load_data(tickers, req.start_date, req.end_date)
 
     results = []
+    max_total_return = 0.0
     for StrategyClass in STRATEGIES:
         strategy = StrategyClass(data, weights, req.initial_cash)
         raw_results = strategy.run()
@@ -25,5 +28,11 @@ def run_backtest(req: BacktestRequest):
                 weights=weights
             )
             results.append(metrics)
-
+            if metrics["total_return"] > max_total_return:
+                max_total_return = max(max_total_return, metrics["total_return"])
+                max_strategy = copy.deepcopy(metrics)
+    max_strategy.pop("portfolio_growth", None)
+    max_strategy.pop("drawdown_series", None)
+    max_strategy.pop("annual_returns", None)
+    update_max_strategy(max_total_return, max_strategy)
     return {"results": results}
