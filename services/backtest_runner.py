2ce2641cab29cc3a0c76ef1ef247e4d2ba884f8e
sehooneom04 <sehooneom@dankook.ac.kr>
2025-05-30 21:21:51 +0900

    🐛fix: 백테스트에 존재하는 각종 버그
    백테스트에 존재하는 각종 버그들을 수정한다.(사용 column으로 인한 값 오류, mdd/낙폭 문제, cagr 0으로 출력되는 문제)
    
    related to #39

--- services/backtest_runner.py
+++ services/backtest_runner.py
@@ -1,29 +1,49 @@
 from app.strategies.buy_and_hold import BuyAndHold
 from app.strategies.sma_cross import SmaCross
-from app.strategies.rsi import Rsi
+from app.strategies.rsi import RSI
 from app.services.data_provider import load_data
 from app.utils.calculate_metrics import calculate_metrics
 from app.models.request_models import BacktestRequest
+from datetime import datetime
+STRATEGY_MIN_DAYS = {
+    "RSI": 14,
+    "SmaCross": 50,
+    "BuyAndHold": 1
+}
 
-STRATEGIES = [BuyAndHold, SmaCross, Rsi]
+STRATEGIES = [BuyAndHold, SmaCross, RSI]
 
 def run_backtest(req: BacktestRequest):
     tickers = [asset.ticker for asset in req.portfolio]
     weights = {asset.ticker: asset.weight / 100 for asset in req.portfolio}
     data = load_data(tickers, req.start_date, req.end_date)
+    start = datetime.strptime(req.start_date, "%Y-%m-%d")
+    end = datetime.strptime(req.end_date, "%Y-%m-%d")
+    date_diff = (end - start).days
 
     results = []
+
     for StrategyClass in STRATEGIES:
-        strategy = StrategyClass(data, weights, req.initial_cash)
-        raw_results = strategy.run()
-        for raw_result in raw_results:
-            metrics = calculate_metrics(
-                raw_result,
-                strategy=raw_result["strategy"],
-                rebalance=raw_result["rebalance"],
-                initial_cash=req.initial_cash,
-                weights=weights
-            )
-            results.append(metrics)
+        try:
+            required_days = STRATEGY_MIN_DAYS[StrategyClass.__name__]
+            if date_diff < required_days:
+                raise ValueError(
+                    f"{StrategyClass.__name__} 전략은 최소 {required_days}일 이상의 데이터가 필요합니다. 현재 기간: {date_diff}일")
+            strategy = StrategyClass(data, weights, req.initial_cash)
+            raw_results = strategy.run()
+            for raw_result in raw_results:
+                metrics = calculate_metrics(
+                    raw_result,
+                    strategy=raw_result["strategy"],
+                    rebalance=raw_result["rebalance"],
+                    initial_cash=req.initial_cash,
+                    weights=weights
+                )
+                results.append(metrics)
+        except ValueError as e:
+            results.append({
+                "strategy": StrategyClass.__name__,
+                "error": str(e)
+            })
 
     return {"results": results}
